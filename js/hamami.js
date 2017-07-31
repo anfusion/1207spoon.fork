26118ec03012c25820e682c3f3547a94591ac875
anfusion <nzmoriarty@gmail.com>
2017-07-31 13:45:19 +0900

    Update hamami.js

--- js/hamami.js
+++ js/hamami.js
@@ -1,11 +1,11 @@
       function initMap() {
         var Hamami = {lat: 38.8360654, lng: 141.5794449};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 17,
           center: Hamami
         });
         var marker = new google.maps.Marker({
-          position: uluru,
+          position: Hamami
           map: map
         });
       }
